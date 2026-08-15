92339e7403ae73cdebaeb1106827de47f278e8b0
Harley Bartles <32537371+HarleyBartles@users.noreply.github.com>
2026-08-15 16:15:50 +0100

    Let Lab 2 worker create database schema

--- labs/02-give-the-cloud-agent-the-project/project/local-setup/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS attendees;
-
-CREATE TABLE attendees (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    confirmed INTEGER NOT NULL CHECK (confirmed IN (0, 1)),
-    meal TEXT NOT NULL CHECK (meal IN ('standard', 'vegetarian', 'vegan'))
-);
